3756273685eea0aab11125fb08d5a69a80800e85
Pavel Novichkov <novickovpavel3@gmail.com>
2026-05-28 21:42:52 +0700

    add security

---
+++ src/main/java/ru/netology/hibernate/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package ru.netology.hibernate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/persons/by-city").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll
+                );
+        return http.build();
+    }
+}
